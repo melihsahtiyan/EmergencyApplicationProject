f6cf326e13fac241426b774a1899d37a0aca019c
Melih <melihsahtiyan@gmail.com>
2022-03-18 12:12:08 +0300

    Commit 2 18.03.2022

---
+++ Business/BusinessAspects/SecuredOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Utilities.Interceptors;
+using Microsoft.AspNetCore.Http;
+
+namespace Business.BusinessAspects
+{
+    public class SecuredOperation : MethodInterception
+    {
+        private string[] _roles;
+        private IHttpContextAccessor _httpContextAccessor;
+
+
+    }
+}
